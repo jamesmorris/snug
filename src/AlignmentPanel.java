0c44585daeac5f93a78028eea1c366fea96eecdd
jm20 <jm20@sanger.ac.uk>
2013-08-21 17:09:34 +0100

    colour bases if different from the reference sequence shade bases on quality

--- src/AlignmentPanel.java
+++ src/AlignmentPanel.java
@@ -1,8 +1,10 @@
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -22,137 +24,147 @@ public class AlignmentPanel extends JPanel {
 	 */
 	private static final long serialVersionUID = 1L;
 	ArrayList<SAMRecord> reads;
+	int pixPerBase;
+	int offset;
+	Variant variant;
+	int trackHeight;
 	IndexedFastaSequenceFile reference;
-	int ALIGNMENT_PIX_PER_BASE;
-	Variant v;
+	// interface options
+	private boolean hideBases;
 	
-	public void setVariant(Variant variant) {
-		this.v = variant;
-	}
-
-	public AlignmentPanel() {
+	public AlignmentPanel(int pixPerBase, int offset, ArrayList<SAMRecord> reads, Variant v, int height, IndexedFastaSequenceFile ref) {
 		super();
-		setPreferredSize(new Dimension(900, 600));
-		setBackground(Color.white);
+		this.pixPerBase = pixPerBase;
+		this.offset = offset;
+		this.reads = reads;
+		this.variant = v;
+		this.trackHeight = height;
+		this.reference = ref;
 	}
 
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D) g;
-
-		FontMetrics fm = getFontMetrics(getFont());
-		ALIGNMENT_PIX_PER_BASE = fm.charWidth('M');
-		
 		drawAlignment(g2);
 	}
-
+	
+	
+	
 	private void drawAlignment(Graphics2D g2) {
-    	
-    	if (reads != null && reference != null) {
-    		
+    	if (reads != null) {    		
     		int ypos = 15;
-    		g2.setColor(Color.blue);
-    		
-    		int width = this.getWidth();
-    		int height = this.getHeight();
-    		// how many bases can we fit in the current window
-    		int visableBases = width / ALIGNMENT_PIX_PER_BASE;
-    		// assume the middle base is the variant - how many bases are either side of it
-    		int surroundingBases = visableBases -1;
-    		// distance in pix to the variant site
-    		int offset = (surroundingBases / 2) * ALIGNMENT_PIX_PER_BASE;
-    		int refStart = v.getPos() - (surroundingBases/2);
-    		int refEnd = v.getPos() + (surroundingBases/2);
-    		
-    		drawRuler(g2, refStart, refEnd, ypos);
-    		ypos += 20;
-    		drawReference(g2, refStart, refEnd, ypos);
-    		
-    		// draw lines before and after to show the position of the variant
-    		g2.drawLine(offset-1, 0, offset-1, height);
-    		g2.drawLine(offset + ALIGNMENT_PIX_PER_BASE, 0, offset + ALIGNMENT_PIX_PER_BASE, height);
-    		
-    		ypos += 15;
     		g2.setColor(Color.black);
             for (SAMRecord read : reads) {            	
             	drawSequence(g2, read, ypos, offset);
             	ypos += 15;
-    		}            
+    		}
+    		// draw lines before and after to show the position of the variant
+            g2.drawLine(offset-2, 0, offset-2, trackHeight);
+    		g2.drawLine(offset + (pixPerBase-2), 0, offset + (pixPerBase-2), trackHeight);
     	}
     }
 	
-	private void drawReference(Graphics2D g2, int refStart, int refEnd, int ypos) {
-		
-		ReferenceSequence refSeq = reference.getSubsequenceAt(v.getChr(), refStart, refEnd);
-		byte[] bases = refSeq.getBases();
-		
-		g2.setColor(Color.blue);
-		for(int i=0; i<bases.length; i++) {
-			String base = String.valueOf((char) bases[i]);
-			g2.drawString(base, i * ALIGNMENT_PIX_PER_BASE, ypos);	
-		}		
-	}
-
-	private void drawRuler(Graphics2D g2, int refStart, int refEnd, int ypos) {
-		
-		ReferenceSequence refSeq = reference.getSubsequenceAt(v.getChr(), refStart, refEnd);
-		byte[] bases = refSeq.getBases();
-		
-		g2.setColor(Color.black);		
-		for(int i=0; i<bases.length; i++) {			
-			if ((i+1) % 10 == 0) {
-				g2.drawString("|", i * ALIGNMENT_PIX_PER_BASE + 2, ypos);
-			} else {
-				g2.drawString("_", i * ALIGNMENT_PIX_PER_BASE + 1, ypos);
-			}
-		}
-	}
-	
 	private void drawSequence(final Graphics2D g2, final SAMRecord samRecord, int ypos, int offset) {
 		
-		int xpos;
+		int xpos = 0;
 		int basePosition;
 		final String readSeq = samRecord.getReadString();
+		
+		FontMetrics fm = g2.getFontMetrics();
+		Rectangle2D rect = fm.getStringBounds("A", g2);
+
+		byte[] baseQualityScores = samRecord.getBaseQualities();
 
 		List<AlignmentBlock> blocks = samRecord.getAlignmentBlocks();
+		
+		// print the strand direction		
+		boolean strand = samRecord.getReadNegativeStrandFlag();
+		
+		if (strand) {
+			samRecord.getAlignmentStart();
+			int distance2Variant = samRecord.getAlignmentStart() - variant.getPos();			
+			basePosition = distance2Variant * pixPerBase;			
+			xpos = (offset + basePosition) - ((int) fm.getStringBounds("< < <", g2).getWidth() + 5);
+			g2.setColor(Color.gray);
+			g2.drawString("< < <", xpos, ypos);			
+		}
 		
 		for(int i=0; i<blocks.size(); i++) {
 			
 			AlignmentBlock block = blocks.get(i);
 			int blockStart = block.getReadStart();
-			int distance2Variant = block.getReferenceStart() - v.getPos();			
+			int distance2Variant = block.getReferenceStart() - variant.getPos();			
 			
 			for(int j=0; j<block.getLength(); j++) {
 			
 				int readPos = blockStart-1+j;
-				int currentPosition = ((distance2Variant + j) + v.getPos());
-				ReferenceSequence refSeq = reference.getSubsequenceAt(v.getChr(), currentPosition, currentPosition);
+				int currentPosition = ((distance2Variant + j) + variant.getPos());
+				
+				ReferenceSequence refSeq = reference.getSubsequenceAt(variant.getChr(), currentPosition, currentPosition);
 				byte[] bases = refSeq.getBases();				
 				String refBase = String.valueOf((char) bases[0]);
 				String readBase = readSeq.substring(readPos, readPos+1);
-				
-				// check read base against reference base - colour or bold if different
-				if (readBase.matches(refBase)) {
-					g2.setColor(Color.black);
-					readBase = "-";
-				} else {
-					g2.setColor(Color.red);
-				}
-				
-				basePosition = (distance2Variant + j) * ALIGNMENT_PIX_PER_BASE;
+				basePosition = (distance2Variant + j) * pixPerBase;
 				xpos = offset + basePosition;
+				
+				// shade the background of the base on quality
+				setColourByBaseQuality(g2, baseQualityScores[readPos]);
+				rect = fm.getStringBounds(readBase, g2);
+				g2.fillRect(xpos, ypos - fm.getAscent(), (int) rect.getWidth(), (int) rect.getHeight());
+				
+				// check read base against reference base - colour if different
+				if (readBase.matches(refBase)) {
+					g2.setColor(Color.gray);					
+				} else {
+					baseColour(g2, readBase);
+				}				
 				g2.drawString(readBase, xpos, ypos);				
-			}
+			}			
+		}
+	
+		// print the strand direction
+		if (!strand) {
+			g2.setColor(Color.gray);
+			g2.drawString("> > >", xpos + pixPerBase, ypos);
+		}
+		
+	}
+
+	private void baseColour(Graphics2D g2, String base) {
+		if (base.equals("A")) {
+			g2.setColor(Color.green);
+		} else if (base.equals("T")) {
+			g2.setColor(Color.red);
+		} else if (base.equals("C")) {
+			g2.setColor(Color.blue);
+		} else if (base.equals("G")) {
+			g2.setColor(Color.orange);
+		} else {
+			g2.setColor(Color.black);
 		}
 	}
 
+	private void setColourByBaseQuality(Graphics2D g2, byte baseQuality) {
+	    if (baseQuality < 10)
+	      g2.setColor(new Color(160, 160, 160));
+	    else if (baseQuality < 20)
+	      g2.setColor(new Color(192, 192, 192));
+	    else if (baseQuality < 30)
+	      g2.setColor(new Color(224, 224, 224));
+	    else
+	    	g2.setColor(Color.white);
+	}
+	
 	public void setReads(ArrayList<SAMRecord> reads) {
 		this.reads = reads;
 	}
 
 	public void setReference(IndexedFastaSequenceFile indexedFastaSequenceFile) {
 		this.reference = indexedFastaSequenceFile;
+	}
 
+	public void setHideBases(boolean b) {
+		this.hideBases = b;		
 	}
 
 }
