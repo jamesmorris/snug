b3bc52d9c720cc9c096cf8eb53a50f19d91c2b3d
jm20 <jm20@sanger.ac.uk>
2013-08-21 17:10:42 +0100

    load multiple bams

--- src/SnugViewer.java
+++ src/SnugViewer.java
@@ -8,6 +8,7 @@ import net.sf.samtools.SAMFileReader;
 import net.sf.samtools.SAMRecord;
 import net.sf.samtools.SAMRecordIterator;
 import net.sf.picard.reference.IndexedFastaSequenceFile;
+import net.sf.picard.reference.ReferenceSequence;
 
 import java.awt.event.*;
 import java.awt.geom.Ellipse2D;
@@ -28,15 +29,20 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.concurrent.TimeUnit;
 
-public class SnugViewer extends JFrame implements ActionListener {
 
+public class SnugViewer extends JFrame implements ActionListener, ComponentListener {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1122484803356474269L;
 	String plottedSNP = null;
 	private File output;
 	private ArrayList<Variant> variantList;
 	private Variant currentVariantinList;
 	private Variant currentVariant = null;
 	private int currentVariantIndex;
-	private int displayVariantIndex;
+	private Integer displayVariantIndex = null;
 	private boolean backVariant = false;
 	private boolean bamFile = false;
 	private boolean variantFile = false;
@@ -45,6 +51,7 @@ public class SnugViewer extends JFrame implements ActionListener {
 	JFileChooser jfc;
 	private JPanel scorePanel;
 	private JPanel messagePanel;
+	private JPanel controlsPanel;
 	private JLabel message;
 	private JButton yesButton;
 	private JButton maybeButton;
@@ -56,18 +63,25 @@ public class SnugViewer extends JFrame implements ActionListener {
 	public static LoggingDialog ld;
 	DataConnectionDialog dcd;
 	
+	JMenuBar mb;
+	
+	// untested below
+	private JPanel trackContainer;
+	
+	private JCheckBoxMenuItem hideBases;
+	
 	JPanel contentPanel;
 
 	SAMFileReader bamReader;
+	private ArrayList<SAMFileReader> bams;
 
-	public enum MouseMode {
-		LASSO, ZOOM
-	};
-
-	private MouseMode mouseMode;
 	private boolean backSNP;
-	private AlignmentPanel ap;
+	
 	private IndexedFastaSequenceFile referenceFile;
+	
+
+	int pixPerBase = 15;
+	
 
 	public static void main(String[] args) {
 
@@ -79,23 +93,27 @@ public class SnugViewer extends JFrame implements ActionListener {
 		super("Snug...");
 
 		dcd = new DataConnectionDialog(this);
-		JMenuBar mb = new JMenuBar();
+		mb = new JMenuBar();
 
 		int menumask = Toolkit.getDefaultToolkit().getMenuShortcutKeyMask();
 
 		fileMenu = new JMenu("File");
 		loadFiles = new JMenuItem("Load Files");
-		loadFiles.setAccelerator(KeyStroke
-				.getKeyStroke(KeyEvent.VK_B, menumask));
+		loadFiles.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_B, menumask));
 		loadFiles.addActionListener(this);
 		fileMenu.add(loadFiles);
-
+		
+		hideBases = new JCheckBoxMenuItem("Hide Bases");
+		hideBases.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F, menumask));
+		hideBases.addActionListener(this);
+		hideBases.setEnabled(false);
+		fileMenu.add(hideBases);		
+		
 		mb.add(fileMenu);
 
 		if (!(System.getProperty("os.name").toLowerCase().contains("mac"))) {
 			JMenuItem quitItem = new JMenuItem("Quit");
-			quitItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q,
-					menumask));
+			quitItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q,menumask));
 			quitItem.addActionListener(this);
 			fileMenu.add(quitItem);
 		}
@@ -108,39 +126,31 @@ public class SnugViewer extends JFrame implements ActionListener {
 
 		setJMenuBar(mb);
 
-		JPanel controlsPanel = new JPanel();
+		controlsPanel = new JPanel();
 
 		scorePanel = new JPanel();
 		scorePanel.add(new JLabel("Approve?"));
 
 		yesButton = new JButton("Yes");
-		scorePanel.registerKeyboardAction(this, "Yes",
-				KeyStroke.getKeyStroke(KeyEvent.VK_Y, 0),
-				JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+		scorePanel.registerKeyboardAction(this, "Yes", KeyStroke.getKeyStroke(KeyEvent.VK_Y, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
 		yesButton.addActionListener(this);
 		yesButton.setEnabled(false);
 		scorePanel.add(yesButton);
 
 		maybeButton = new JButton("Maybe");
-		scorePanel.registerKeyboardAction(this, "Maybe",
-				KeyStroke.getKeyStroke(KeyEvent.VK_M, 0),
-				JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+		scorePanel.registerKeyboardAction(this, "Maybe", KeyStroke.getKeyStroke(KeyEvent.VK_M, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
 		maybeButton.addActionListener(this);
 		maybeButton.setEnabled(false);
 		scorePanel.add(maybeButton);
 
 		noButton = new JButton("No");
-		scorePanel.registerKeyboardAction(this, "No",
-				KeyStroke.getKeyStroke(KeyEvent.VK_N, 0),
-				JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+		scorePanel.registerKeyboardAction(this, "No", KeyStroke.getKeyStroke(KeyEvent.VK_N, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
 		noButton.addActionListener(this);
 		noButton.setEnabled(false);
 		scorePanel.add(noButton);
 
 		backButton = new JButton("Back");
-		scorePanel.registerKeyboardAction(this, "Back",
-				KeyStroke.getKeyStroke(KeyEvent.VK_B, 0),
-				JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+		scorePanel.registerKeyboardAction(this, "Back", KeyStroke.getKeyStroke(KeyEvent.VK_B, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
 		backButton.addActionListener(this);
 		backButton.setEnabled(false);
 		scorePanel.add(backButton);
@@ -157,19 +167,20 @@ public class SnugViewer extends JFrame implements ActionListener {
 		rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
 		controlsPanel.add(rightPanel);
 
-		controlsPanel.setMaximumSize(new Dimension(2000, (int) controlsPanel
-				.getPreferredSize().getHeight()));
-		controlsPanel.setMinimumSize(new Dimension(10, (int) controlsPanel
-				.getPreferredSize().getHeight()));
+		controlsPanel.setMaximumSize(new Dimension(2000, (int) controlsPanel.getPreferredSize().getHeight()));
+		controlsPanel.setMinimumSize(new Dimension(10, (int) controlsPanel.getPreferredSize().getHeight()));
 
-		mouseMode = MouseMode.ZOOM;
-
-		ap = new AlignmentPanel();
+		trackContainer = new JPanel();
+		trackContainer.setPreferredSize(new Dimension(900, 600));
+		trackContainer.setBackground(Color.white);
+		
 		
 		contentPanel = new JPanel();
 		contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
-		contentPanel.add(ap);
+		contentPanel.add(trackContainer);
 		contentPanel.add(controlsPanel);
+		contentPanel.addComponentListener(this);
+		
 
 		addWindowListener(new WindowAdapter() {
 
@@ -181,9 +192,6 @@ public class SnugViewer extends JFrame implements ActionListener {
 		this.setContentPane(contentPanel);
 		this.pack();
 		this.setVisible(true);
-
-		// add a panel and to it add a text string for test
-		ap.repaint();
 
 		ld = new LoggingDialog(this);
 		ld.pack();
@@ -198,6 +206,7 @@ public class SnugViewer extends JFrame implements ActionListener {
 		String openFilesCommand = new String("Load Files");
 		String showLogCommand = new String("Show log");
 		String quitCommand = new String("Quit");
+		String hideBasesCommand = "Hide Bases";
 
 		String command = actionEvent.getActionCommand();
 		if (command.equals(noCommand)) {
@@ -241,45 +250,75 @@ public class SnugViewer extends JFrame implements ActionListener {
 			drawReads();
 		} else if (command.equals(openFilesCommand)) {
 
+			bams = new ArrayList<SAMFileReader>();
+			
 			dcd.pack();
 			dcd.setVisible(true);
 
 			// check all the dialog fields are valid paths to data
 			
 			String bam = dcd.getBam();
-			File bamFile = new File(bam);
-
-			if (bamFile.exists()) {
-				bamReader = new SAMFileReader(bamFile);
-			}
-
-			File bamIndexFile = new File(bam + ".bai");
-
-			if (!bamIndexFile.exists()) {
-				String ls = System.getProperty("line.separator");
-				String msg = "BAM index file is missing. The BAM file needs to be sorted and indexed"
-						+ ls
-						+ "This can be done using samtools (http://samtools.sf.net/):"
-						+ ls
-						+ ls
-						+ "samtools sort <in.bam> <out.prefix>"
-						+ ls
-						+ "samtools index <sorted.bam>";
-				ld.log(msg);
-			} else {
-				ld.log("BAM loaded");
+			
+			// if the bam path ends .bam just open the single file			
+			if (bam.endsWith(".bam")) {
+				System.out.println(bam);
+				File bamFile = new File(bam);
+				if (bamFile.exists() && checkBamIndex(bam)) {
+					bams.add(new SAMFileReader(bamFile));
+					bamReader = new SAMFileReader(bamFile);
+				}
+			
+			// if the bam path ends .bams the file is assumed to contain a list of bam files				
+			} else if (bam.endsWith(".bams")) {
+				
+				File bamFiles = new File(bam);
+				String absolutePath = bamFiles.getAbsolutePath();
+				String bamDir = absolutePath.substring(0,absolutePath.lastIndexOf(File.separator));
+				
+				// open the bams file and loop through all the bam files
+				BufferedReader listReader = null;
+				try {
+					listReader = new BufferedReader(new FileReader(bam));
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				String currentline;
+				
+				try {
+					while ((currentline = listReader.readLine()) != null) {
+						//check that the line is not blank
+						if (currentline == null || currentline.trim().equals("")){
+							continue;
+						} else {
+							String b = bamDir +File.separator + currentline.trim();
+							File bamFile = new File(b);
+							if (bamFile.exists() && checkBamIndex(b)) {
+								bams.add(new SAMFileReader(bamFile));
+							}
+						}
+					}
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 
 			currentVariantIndex = 0;
 			displayVariantIndex = currentVariantIndex;
+			
+			// load variants file
 			String var = dcd.getVar();
 			VariantListFile variantListFile = new VariantListFile(var);
 			variantList = variantListFile.getVariants();
 			ld.log(variantList.size() + " variants loaded");
 			output = checkOverwriteFile(new File(var + ".scores"));
 			
+			
+			// load the reference file			
 			try {
-				ap.setReference(new IndexedFastaSequenceFile(new File(dcd.getRef())));
+				referenceFile = new IndexedFastaSequenceFile(new File(dcd.getRef()));
 				ld.log("Reference file loaded");
 			} catch (FileNotFoundException e) {
 				ld.log("Reference file not found");
@@ -289,45 +328,107 @@ public class SnugViewer extends JFrame implements ActionListener {
 			noButton.setEnabled(true);
 			maybeButton.setEnabled(true);
 			yesButton.requestFocusInWindow();
-			drawReads();			
+			drawReads();
+//			this.pack();
+			
 				
 		} else if (command.equals(quitCommand)) {
 			System.exit(0);
 		} else if (command.equals(showLogCommand)) {
 			ld.setVisible(true);
+		} else if (command.equals(hideBasesCommand)) {
+            // turn filtering on/off
+            if (hideBases.isSelected()) {
+                hideBases();
+            } else {
+                showBases();
+            }
 		}
+		
+	}
+
+	private void showBases() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void hideBases() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private Boolean checkBamIndex(String bam) {
+		
+		File bamIndexFile = new File(bam + ".bai");
+
+		if (!bamIndexFile.exists()) {
+			String ls = System.getProperty("line.separator");
+			String msg = "BAM index file is missing. The BAM file needs to be sorted and indexed"
+					+ ls
+					+ "This can be done using samtools (http://samtools.sf.net/):"
+					+ ls
+					+ ls
+					+ "samtools sort <in.bam> <out.prefix>"
+					+ ls
+					+ "samtools index <sorted.bam>";
+			ld.log(msg);
+			return Boolean.FALSE;
+		} else {
+			ld.log("BAM loaded: " + bam);
+			return Boolean.TRUE;	
+		}
+		
 	}
 
 	private void drawReads() {
 		Variant v = variantList.get(displayVariantIndex);
-		ArrayList<SAMRecord> reads = new ArrayList<SAMRecord>();		
-		SAMRecordIterator readsItr = bamReader.queryOverlapping(v.getChr(), v.getPos(), v.getPos());
+		this.setTitle(v.getName());
 		
-        while (readsItr.hasNext()) {	
-        	reads.add(readsItr.next());  	
-        }
-        readsItr.close();
-        
-        Integer region_start = null;
-		Integer region_end = 0;
+		trackContainer.removeAll();
+
+		// calculate the height of each panel		
+		int refPanelHeight = 60;
+		int border = 40;
+		int height = this.getHeight() - (controlsPanel.getHeight() + mb.getHeight()) - refPanelHeight - border;
+		int trackHeight = height / bams.size();
+		int width = this.getWidth() - border;
+		int visableBases = width / pixPerBase;
 		
-		for (SAMRecord read : reads) {
-			if (region_start == null) {
-				region_start = read.getAlignmentStart();
-			}
-			if (read.getAlignmentEnd() > region_end) {
-				region_end = read.getAlignmentEnd();
-			}
+		// assume the middle base is the variant - how many bases are either side of it
+		int basesOneSide = (visableBases -1) / 2;
+		
+		// distance in pix to the variant site
+		int offset = basesOneSide * pixPerBase;
+				
+		for (SAMFileReader sfr : bams) {
+			
+			ArrayList<SAMRecord> reads = new ArrayList<SAMRecord>();		
+			SAMRecordIterator readsItr = sfr.queryOverlapping(v.getChr(), v.getPos(), v.getPos());
+			
+			while (readsItr.hasNext()) {	
+	        	reads.add(readsItr.next());  	
+	        }
+	        readsItr.close();
+	        
+	        AlignmentPanel ap = new AlignmentPanel(pixPerBase, offset, reads, v, height, referenceFile);
+	        
+	        ap.setPreferredSize(new Dimension(width, trackHeight));
+			setBackground(Color.white);
+			ap.setBorder(BorderFactory.createLineBorder(Color.black));	        
+			trackContainer.add(ap);
+
 		}
 		
-		ap.setReads(reads);
-		ap.setVariant(v);
-		ap.repaint();
+		// draw the reference panel
+		int refStart = v.getPos() - basesOneSide;
+		int refEnd = v.getPos() + basesOneSide;
 
-		this.pack();
+		ReferencePanel rp = new ReferencePanel(pixPerBase, v, referenceFile, refStart, refEnd);
+		rp.setPreferredSize(new Dimension(width, refPanelHeight));
+		trackContainer.add(rp);
+		
 		this.setVisible(true);
-		this.setTitle(v.getName());
-
+		
 	}
 
 	private void disableAllActions() {
@@ -378,14 +479,6 @@ public class SnugViewer extends JFrame implements ActionListener {
 
 	private boolean isBackSNP() {
 		return backSNP;
-	}
-
-	protected MouseMode getMouseMode() {
-		return mouseMode;
-	}
-
-	protected void setMouseMode(MouseMode mm) {
-		this.mouseMode = mm;
 	}
 
 	private File checkOverwriteFile(File file) {
@@ -453,8 +546,33 @@ public class SnugViewer extends JFrame implements ActionListener {
 	}
 
 	public void refreshPlot() {
-		if (currentVariant != null) {
+		if (displayVariantIndex != null) {
 			drawReads();
 		}
+	}
+
+	@Override
+	public void componentHidden(ComponentEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void componentMoved(ComponentEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void componentResized(ComponentEvent arg0) {
+		if (displayVariantIndex != null) {
+			drawReads();
+		}
+	}
+
+	@Override
+	public void componentShown(ComponentEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 }
