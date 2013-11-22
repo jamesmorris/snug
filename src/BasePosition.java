604305b2a4b8acfa882c4a1ebba7215cea30d1ec
jm20 <jm20@sanger.ac.uk>
2013-11-22 16:22:11 +0000

    added the ability to record base counts on both strands

--- src/BasePosition.java
+++ src/BasePosition.java
@@ -1,55 +1,98 @@
 
 public class BasePosition {
-	int a = 0;
-	int t = 0;
-	int g = 0;
-	int c = 0;
+	int ap = 0;
+	int tp = 0;
+	int gp = 0;
+	int cp = 0;
+	int am = 0;
+	int tm = 0;
+	int gm = 0;
+	int cm = 0;
 	
-	public int getA() {
-		return a;
+	public int getAplus() {
+		return ap;
 	}
 
-	public void setA(int a) {
-		this.a = a;
+	public void setAplus(int a) {
+		this.ap = a;
 	}
 
-	public int getT() {
-		return t;
+	public int getTplus() {
+		return tp;
 	}
 
-	public void setT(int t) {
-		this.t = t;
+	public void setTplus(int t) {
+		this.tp = t;
 	}
 
-	public int getG() {
-		return g;
+	public int getGplus() {
+		return gp;
 	}
 
-	public void setG(int g) {
-		this.g = g;
+	public void setGplus(int g) {
+		this.gp = g;
 	}
 
-	public int getC() {
-		return c;
+	public int getCplus() {
+		return cp;
 	}
 
-	public void setC(int c) {
-		this.c = c;
+	public void setCplus(int c) {
+		this.cp = c;
 	}
 
 	public BasePosition() {
 		
 	}
+	public int getAminus() {
+		return am;
+	}
 
+	public void setAminus(int a) {
+		this.am = a;
+	}
+
+	public int getTminus() {
+		return tm;
+	}
+
+	public void setTminus(int t) {
+		this.tm = t;
+	}
+
+	public int getGminus() {
+		return gm;
+	}
+
+	public void setGminus(int g) {
+		this.gm = g;
+	}
+
+	public int getCminus() {
+		return cm;
+	}
+
+	public void setCminus(int c) {
+		this.cm = c;
+	}
+	
 	public void addBase(String base) {
-		if (base.equals("A")) {
-			setA(getA() + 1);
-		} else if (base.equals("T")) {
-			setT(getT() + 1);
-		} else if (base.equals("C")) {
-			setC(getC() + 1);
-		} else if (base.equals("G")) {
-			setG(getG() + 1);
-		} 
+		if (base.equals("+A")) {
+			setAplus(getAplus() + 1);
+		} else if (base.equals("+T")) {
+			setTplus(getTplus() + 1);
+		} else if (base.equals("+C")) {
+			setCplus(getCplus() + 1);
+		} else if (base.equals("+G")) {
+			setGplus(getGplus() + 1);
+		} else if (base.equals("-A")) {
+			setAminus(getAminus() + 1);
+		} else if (base.equals("-T")) {
+			setTminus(getTminus() + 1);
+		} else if (base.equals("-C")) {
+			setCminus(getCminus() + 1);
+		} else if (base.equals("-G")) {
+			setGminus(getGminus() + 1);
+		}  
 	}
 }
